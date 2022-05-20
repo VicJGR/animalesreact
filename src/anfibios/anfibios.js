fa9cfaa4a7d956ea4d377d796c4d15a074ac63c2
vazquezpmarcos <vazquezpmarcos@gmail.com>
2022-05-20 13:35:01 +0200

    anfibios

---
+++ src/anfibios/anfibios.js
@@ -0,0 +1,45 @@
+function App(){
+    return(
+<>
+    <div>
+        <a href="./Aves.html">Aves</a>
+        <a href="./Mamiferos.html">Mamiferos</a>
+        <a href="./reptiles.html">Reptiles</a>
+        <a href="./peces.html">Peces</a>
+    </div>
+   <br/>
+    <div>
+        <h1>Anfibios</h1>
+        <div>
+            <p>Rana</p>
+            <img src="./imagenes/rana.jpg"alt=""/>
+        </div>
+        <div>
+            <p>Sapo</p>
+            <img src="./imagenes/bufo_bufo_03.jpg"alt=""/>
+        </div>
+        <div>
+            <p>Axolote</p>
+            <img src="./imagenes/axolote_02.jpg"alt=""/>
+        </div>
+        <br/>
+        <div>
+            <p>Cecilia</p>
+            <img src="./imagenes/cecilia.jpg"alt=""/>
+        </div>
+        <div>
+            <p>Salamandra</p>
+            <img src="./imagenes/salmandra.jpg"alt=""/>
+        </div>
+        <div>
+            <p>Tritón</p>
+            <img src="./imagenes/tritón.jpg"alt=""/>
+        </div>
+    </div>
+    <div>
+    <br/>
+        <a class="bottom"href="./index.html">Volver</a>
+        <br/>
+    </div>
+</>
+    )};
