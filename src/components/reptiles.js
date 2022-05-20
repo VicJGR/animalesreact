3b2b91e8f37ef509f518bf5d19137970f4665cc7
vazquezpmarcos <vazquezpmarcos@gmail.com>
2022-05-20 13:04:04 +0200

    alt

--- src/components/reptiles.js
+++ src/components/reptiles.js
@@ -16,27 +16,27 @@ function App(){
 
 <div class="gallery">
   <figure class="galleryItem">
-      <img src="./imagenes/zauneidechse1.jpg" class="galleryImg"/>
+      <img src="./imagenes/zauneidechse1.jpg" class="galleryImg" alt=""/>
       <figcaption>Lacerta agilis</figcaption>
   </figure>
   <figure class="galleryItem">
-      <img src="./imagenes/lycaenops.jpg" class="galleryImg"/>
+      <img src="./imagenes/lycaenops.jpg" class="galleryImg" alt=""/>
       <figcaption>Lycaenops</figcaption>
   </figure>
   <figure class="galleryItem">
-      <img src="./imagenes/bradysaurus.jpg" class="galleryImg"/>
+      <img src="./imagenes/bradysaurus.jpg" class="galleryImg" alt=""/>
       <figcaption>Bradysaurus</figcaption>
   </figure>
   <figure class="galleryItem">
-      <img src="./imagenes/hylonomus.jpg" class="galleryImg"/>
+      <img src="./imagenes/hylonomus.jpg" class="galleryImg" alt=""/>
       <figcaption>Hylonomus</figcaption>
   </figure>
   <figure class="galleryItem">
-      <img src="./imagenes/indianGharial.jpg" class="galleryImg"/>
+      <img src="./imagenes/indianGharial.jpg" class="galleryImg" alt=""/>
       <figcaption>Gavialis gangeticus</figcaption>
   </figure>
   <figure class="galleryItem">
-      <img src="./imagenes/iguanaIguana.jpg" class="galleryImg"/>
+      <img src="./imagenes/iguanaIguana.jpg" class="galleryImg" alt=""/>
       <figcaption>Iguana iguana</figcaption>
   </figure>
 
