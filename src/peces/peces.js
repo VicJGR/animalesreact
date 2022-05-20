bd8d9aad532ccb7cf81b7168af662378a7398a83
vazquezpmarcos <vazquezpmarcos@gmail.com>
2022-05-20 13:22:47 +0200

    fix reptiles + peces

---
+++ src/peces/peces.js
@@ -0,0 +1,55 @@
+function App(){
+    return(
+<>
+    <div class="align"/>
+    <div>
+    <h4>Menú</h4>
+    <a class="a" href="./aves.html">Aves</a> 
+    <a class="a" href="./reptiles.html">Reptiles</a>
+    <a class="a"  href="./mamiferos.html">Mamíferos</a>
+    <a class="a"  href="./anfibios.html">Anfibios</a>
+    <a class="a"  href="./peces.html">Peces</a>
+</div>    
+<br/>    
+<div>
+    <h1>Peces</h1>
+</div>
+<div>
+    <p>Salmón</p>
+    <img class="image-div" src="./imagenes/salmon.jfif"alt=""/>
+</div>
+<div>
+    <p>Atún</p>
+    <img class="image-div" src="./imagenes/atun.jfif"alt=""/>
+</div>
+<div>
+    <p> Trucha  </p>
+    <img class="image-div" src="./imagenes/trucha.jfif"alt=""/>
+</div>
+<br/>
+<div>
+    <p>Piraña  </p>
+    <img class="image-div" src="./imagenes/piraña.jfif"alt=""/>
+</div>
+<div>
+    <p> Bagre  </p>
+    <img class="image-div" src="./imagenes/bagre.jfif"alt=""/>   
+</div>
+<div>
+    <p> Dorada  </p>
+    <img class="image-div" src="./imagenes/dorada.jfif" alt=""/> 
+</div>
+<br/>
+<br/>
+<div>
+    <footer class="volver">
+        <a class="button" href="./index.html">Volver</a>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+    </footer>
+</div>
+</>
+    )};
+    
