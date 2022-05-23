ff3022515795e052a511c5026f91297e5d5fc55e
Adrix8 <adrianperezpascual@gmail.com>
2022-05-23 11:53:12 +0200

    imagenes anfibios

--- src/components/anfibios/anfibios.js
+++ src/components/anfibios/anfibios.js
@@ -10,28 +10,28 @@ export default function AnfibiosMain(){
         <h1>Anfibios</h1>
         <div>
             <p>Rana</p>
-            <img src="./imagenes/rana.jpg"alt=""/>
+            <img src="./public/img/img/rana.jpg"alt=""/>
         </div>
         <div>
             <p>Sapo</p>
-            <img src="./imagenes/bufo_bufo_03.jpg"alt=""/>
+            <img src="./public/img/img/bufo_bufo_03.jpg"alt=""/>
         </div>
         <div>
             <p>Axolote</p>
-            <img src="./imagenes/axolote_02.jpg"alt=""/>
+            <img src="./public/img/img/axolote_02.jpg"alt=""/>
         </div>
         <br/>
         <div>
             <p>Cecilia</p>
-            <img src="./imagenes/cecilia.jpg"alt=""/>
+            <img src="./public/img/img/cecilia.jpg"alt=""/>
         </div>
         <div>
             <p>Salamandra</p>
-            <img src="./imagenes/salmandra.jpg"alt=""/>
+            <img src="./public/img/img/salmandra.jpg"alt=""/>
         </div>
         <div>
             <p>Tritón</p>
-            <img src="./imagenes/tritón.jpg"alt=""/>
+            <img src="./public/img/img/tritón.jpg"alt=""/>
         </div>
     </div>
     
