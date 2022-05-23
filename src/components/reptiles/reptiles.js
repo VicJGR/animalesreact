310800a8e26088d29b828e506bde758921c3ca05
Maru <marianalopezeikilis>
2022-05-23 13:08:35 +0200

    corregidas las cagadas de reptiles

--- src/components/reptiles/reptiles.js
+++ src/components/reptiles/reptiles.js
@@ -1,24 +1,25 @@
 import "./reptiles.css"
+import Animal from "./../../Animal";
 import {Link} from "react-router-dom";
 
 const reptilesArray = [{
         nombre: "Lacerta agilis",
-        src: "./public/img/img/zauneidechse1.jpg"
+        src: "./img/img/zauneidechse1.jpg"
     },{
         nombre: "Lycaenops",
-        src: "./public/img/img/lycaenops.jpg"
+        src: "./img/img/lycaenops.jpg"
     },{
         nombre: "Bradysaurus",
-        src: "./public/img/img/bradysaurus.jpg"
+        src: "./img/img/bradysaurus.jpg"
     },{
         nombre: "Hylonomus",
-        src: "./public/img/img/hylonomus.jpg"
+        src: "./img/img/hylonomus.jpg"
     },{
         nombre: "Gavialis gangeticus",
-        src: "./public/img/img/indianGharial.jpg"
+        src: "./img/img/indianGharial.jpg"
     },{
         nombre: "Iguana iguana",
-        src: "./public/img/img/iguanaIguana.jpg"
+        src: "./img/img/iguanaIguana.jpg"
     }]
 
 export default function ReptilesMain(){
@@ -31,42 +32,15 @@ export default function ReptilesMain(){
         <Link to ="/peces">Peces</Link>|{" "}
         <Link to ="/reptiles">Reptiles</Link>|{" "}
         </nav>
-  <div class="headerLinks">
-  
-</div>
-<div class="header">
-  <h1>Reptiles</h1>
-</div>
-
-<div class="gallery">
-  <figure class="galleryItem">
-      <img src="./imagenes/zauneidechse1.jpg" class="galleryImg" alt=""/>
-      <figcaption>Lacerta agilis</figcaption>
-  </figure>
-  <figure class="galleryItem">
-      <img src="./imagenes/lycaenops.jpg" class="galleryImg" alt=""/>
-      <figcaption>Lycaenops</figcaption>
-  </figure>
-  <figure class="galleryItem">
-      <img src="./imagenes/bradysaurus.jpg" class="galleryImg" alt=""/>
-      <figcaption>Bradysaurus</figcaption>
-  </figure>
-  <figure class="galleryItem">
-      <img src="./imagenes/hylonomus.jpg" class="galleryImg" alt=""/>
-      <figcaption>Hylonomus</figcaption>
-  </figure>
-  <figure class="galleryItem">
-      <img src="./imagenes/indianGharial.jpg" class="galleryImg" alt=""/>
-      <figcaption>Gavialis gangeticus</figcaption>
-  </figure>
-  <figure class="galleryItem">
-      <img src="./imagenes/iguanaIguana.jpg" class="galleryImg" alt=""/>
-      <figcaption>Iguana iguana</figcaption>
-  </figure>
-
-  <div class="returnButton">
-      <a href="index.html">Volver</a>
-  </div>
-</div>
+<br/>
+        <div class="divGrande"/>
+            <h1>Reptiles</h1>
+            {
+            reptilesArray.map((reptilesArray)=>{
+              return <Animal animal= {reptilesArray}/>
+            })}
+        <footer>
+        <Link to ="/" >Volver</Link>|{" "}
+        </footer>
 </>
     )};
