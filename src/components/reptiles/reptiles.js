fef51298ad1306369e1e8dba9cb9b042175dc1d4
vazquezpmarcos <vazquezpmarcos@gmail.com>
2022-05-23 12:48:47 +0200

    REPTILES POLTERGEIST

--- src/components/reptiles/reptiles.js
+++ src/components/reptiles/reptiles.js
@@ -1,64 +1,38 @@
 import "./reptiles.css"
+import Animal from "./../../Animal";
 
 const reptilesArray = [{
         nombre: "Lacerta agilis",
-        src: "./imagenes/zauneidechse1.jpg"
+        src: "./img/img/zauneidechse1.jpg"
     },{
         nombre: "Lycaenops",
-        src: "./imagenes/lycaenops.jpg"
+        src: "./img/img/lycaenops.jpg"
     },{
         nombre: "Bradysaurus",
-        src: "./imagenes/bradysaurus.jpg"
+        src: "./img/img/bradysaurus.jpg"
     },{
         nombre: "Hylonomus",
-        src: "./imagenes/hylonomus.jpg"
+        src: "./img/img/hylonomus.jpg"
     },{
         nombre: "Gavialis gangeticus",
-        src: "./imagenes/indianGharial.jpg"
+        src: "./img/img/indianGharial.jpg"
     },{
         nombre: "Iguana iguana",
-        src: "./imagenes/iguanaIguana.jpg"
+        src: "./img/img/iguanaIguana.jpg"
     }]
 
 export default function ReptilesMain(){
     return(
 <>
-  <div class="headerLinks">
-  
-</div>
-<div class="header">
-  <h1>Reptiles</h1>
-</div>
-
-<div class="gallery">
-  <figure class="galleryItem">
-      <img src="./imagenes/zauneidechse1.jpg" class="galleryImg" alt=""/>
-      <figcaption>Lacerta agilis</figcaption>
-  </figure>
-  <figure class="galleryItem">
-      <img src="./imagenes/lycaenops.jpg" class="galleryImg" alt=""/>
-      <figcaption>Lycaenops</figcaption>
-  </figure>
-  <figure class="galleryItem">
-      <img src="./imagenes/bradysaurus.jpg" class="galleryImg" alt=""/>
-      <figcaption>Bradysaurus</figcaption>
-  </figure>
-  <figure class="galleryItem">
-      <img src="./imagenes/hylonomus.jpg" class="galleryImg" alt=""/>
-      <figcaption>Hylonomus</figcaption>
-  </figure>
-  <figure class="galleryItem">
-      <img src="./imagenes/indianGharial.jpg" class="galleryImg" alt=""/>
-      <figcaption>Gavialis gangeticus</figcaption>
-  </figure>
-  <figure class="galleryItem">
-      <img src="./imagenes/iguanaIguana.jpg" class="galleryImg" alt=""/>
-      <figcaption>Iguana iguana</figcaption>
-  </figure>
-
-  <div class="returnButton">
-      <a href="index.html">Volver</a>
-  </div>
-</div>
+<br/>
+        <div class="divGrande"/>
+            <h1>Reptiles</h1>
+            {
+            reptilesArray.map((reptilesArray)=>{
+              return <Animal animal= {reptilesArray}/>
+            })}
+        <footer>
+            <a class="volver" href="index.html">Volver</a>
+        </footer>
 </>
     )};
