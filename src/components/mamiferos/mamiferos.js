366cec613229b2fdacd154e21e134bae1452d196
VicJGR <vjgomezrios@gmail.com>
2022-05-23 12:12:17 +0200

    cambio mamiferos

--- src/components/mamiferos/mamiferos.js
+++ src/components/mamiferos/mamiferos.js
@@ -4,42 +4,44 @@ import './mamiferos.css'
 
 
 export default function MamiferosMain() {
-   
-    return (
+    
+    const mamiferosArray = [
+       {
+         nombre: 'Tigre',
+         src: "imagenes/tigre.jpg"
+       },
+       {
+        nombre: 'Lobo',
+        src: "imagenes/lobo.jpg"
+      },
+      {
+        nombre: 'Leon',
+        src: "imagenes/leon.jpg"
+      },
+      {
+        nombre: 'Oso',
+        src: "imagenes/leon.jpg"
+      },
+      {
+        nombre: 'Koala',
+        src: "imagenes/leon.jpg"
+      },
+      {
+        nombre: 'Gato',
+        src: "imagenes/leon.jpg"
+      }  ]
+
+return (
 <>
 
     
         <br/>
-        <div class="divGrande">
+        <div class="divGrande"/>
             <h1>Mamíferos</h1>
-            <div class="card">
-                <p>Osos</p>
-                <img src="./img/img/oso.jpg" alt=""/>
-            </div>
-            <div class="card">
-                <p>Tigre</p>
-                <img src="./img/img/tigre.jpg"alt=""/>
-            </div>
-            <div class="card">
-                <p>Lobo</p>
-                <img src="./img/img/lobo.jpg"alt=""/>
-            </div>
-            <br/>
-            <div class="card">
-                <p>Leon</p>
-                <img src="./img/img/leon.jpg"alt=""/>
-            </div>
-            <div class="card">
-                <p>Koala</p>
-                <img src="./img/img/koala.jpg"alt=""/>
-            </div>
-            <div class="card">
-                <p>Gato</p>
-                <img src="./img/img/Gato.jpg"alt=""/>
-            </div>
-        </div>
-        <br/>
-        <br/>
+            {
+            mamiferosArray.map((mamiferosArray)=>{
+              return <Mamiferos mamiferos= {mamiferosArray}/>
+            })}
         <footer>
             <a class="volver" href="index.html">Volver</a>
         </footer>
