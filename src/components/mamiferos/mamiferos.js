22f041a9382a65673dfae2ae2fc0d72025deb497
VicJGR <vjgomezrios@gmail.com>
2022-05-23 11:54:29 +0200

    Links imagenes mamiferos

--- src/components/mamiferos/mamiferos.js
+++ src/components/mamiferos/mamiferos.js
@@ -14,33 +14,34 @@ export default function MamiferosMain() {
             <h1>Mamíferos</h1>
             <div class="card">
                 <p>Osos</p>
-                <img src="./img/oso.jpg" alt=""/>
+                <img src="./img/img/oso.jpg" alt=""/>
             </div>
             <div class="card">
                 <p>Tigre</p>
-                <img src="./img/tigre.jpg"alt=""/>
+                <img src="./img/img/tigre.jpg"alt=""/>
             </div>
             <div class="card">
                 <p>Lobo</p>
-                <img src="./img/lobo.jpg"alt=""/>
+                <img src="./img/img/lobo.jpg"alt=""/>
             </div>
             <br/>
             <div class="card">
                 <p>Leon</p>
-                <img src="./img/leon.jpg"alt=""/>
+                <img src="./img/img/leon.jpg"alt=""/>
             </div>
             <div class="card">
                 <p>Koala</p>
-                <img src="./img/koala.jpg"alt=""/>
+                <img src="./img/img/koala.jpg"alt=""/>
             </div>
             <div class="card">
                 <p>Gato</p>
-                <img src="./img/Gato.jpg"alt=""/>
+                <img src="./img/img/Gato.jpg"alt=""/>
             </div>
         </div>
         <br/>
         <br/>
         <footer>
+            
             <a class="volver" href="index.html">Volver</a>
         </footer>
         </>
